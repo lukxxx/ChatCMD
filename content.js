a5f6ed15ee2b813740a1c2f3853ecc9abe4f4897
Lukáš Jahoda <lukas.jahoda@protonmail.com>
2023-07-06 17:03:37 +0200

    removed deprecated js keyCode property

--- content.js
+++ content.js
@@ -1,13 +1,19 @@
-document.addEventListener('keydown', function (event) {
-  let ctrl = event.ctrlKey ? event.ctrlKey : ((event.keyCode === 17) ? true : false);
-  if (event.keyCode == '38') {
+document.addEventListener("keydown", function (event) {
+  let ctrl = event.ctrlKey
+    ? event.ctrlKey
+    : event.key === "Control"
+    ? true
+    : false;
+  if (event.key === "ArrowUp") {
     let msgsArr = document.getElementsByClassName("empty:hidden");
-    let prevMsg = msgsArr[msgsArr.length - 1].innerHTML
+    let prevMsg = msgsArr[msgsArr.length - 1].innerHTML;
     let textArea = document.getElementById("prompt-textarea");
     textArea.value += prevMsg;
   }
-  if (event.keyCode == 67 && ctrl) {
-    let btnNeutral = document.getElementsByClassName("btn-neutral")[0]
-    btnNeutral.click();
- }
+  if (event.key === "c" && ctrl) {
+    let btnNeutral = document.getElementsByClassName("btn-neutral")[0];
+    if (btnNeutral.children[0].innerText == "Stop generating") {
+      btnNeutral.click();
+    }
+  }
 });
